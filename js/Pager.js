2ffec8ede65688353501c084e510ea315dc0b225
hzzhu_zhenyu <zzy7186@163.com>
2015-05-27 18:29:41 +0800

    分页代码

--- js/Pager.js
+++ js/Pager.js
@@ -10,10 +10,10 @@
     _pro = Pager.prototype;
     Pager.VERSION = '0.1.0';
     Pager.DEFAULTS = {
-        curno: 9, //当前页码
+        curno: 4, //当前页码
         limit: 10,
         show: 7,
-        total: 8, //总页码
+        total: 20, //总页码
         totalRecords: 0, //总数据条数
         isShowFirstPageBtn: true, //是否显示首页按钮
         isShowLastPageBtn: true, //是否显示尾页按钮
@@ -52,6 +52,7 @@
             this.pager = $('<div class="m-pager"></div>');
             this.pageNoWraper = $('<span class="pages"></span>');
             this.__generateHtml();
+            this.__bindEvent();
         }
     };
 
@@ -68,14 +69,16 @@
         _prevPageBtn = $('<a>').text(this.lang.prePageText)
             .attr({
                 href : 'javascript:void(0);',
-                title : this.lang.prePageTipText
-            });
+                title : this.lang.prePageTipText,
+                "data-action" : "prev"
+            }).addClass('prev');
         if (this.opts.isShowFirstPageBtn) {
             _firstPageBtn = $('<a>').text(this.lang.firstPageText)
                 .attr({
                     href : 'javascript:void(0);',
-                    title: this.lang.firstPageTipText
-                });
+                    title: this.lang.firstPageTipText,
+                    "data-action" : 'first'
+                }).addClass('first');
         }
         if(this.opts.curno == 1) {
             _prevPageBtn.addClass('disabled');
@@ -92,8 +95,10 @@
             for(var i = 1; i <= _opts.total; i++) {
                 _pageNoBtn = $('<a>').text(i).attr({
                     href : 'javascript:void(0);',
-                    title : '第' + i + '页'
-                });
+                    title : '第' + i + '页',
+                    "data-pageno" : i,
+                    "data-action" : "pageno"
+                }).addClass('pageno');
                 if(i == _opts.curno) {
                     _pageNoBtn.addClass('active');
                 }
@@ -111,25 +116,27 @@
                 _end = _opts.total;
             }
             if(_begin > 2) {
-                this.pageNoWraper.append('<a href="javascript:void(0);" title="第一页">1</a>');
-                this.pageNoWraper.append('<a href="javascript:void(0);" title="">...</a>');
+                this.pageNoWraper.append('<a class="pageno" data-action="pageno" data-pageno="1" href="javascript:void(0);" title="第1页">1</a>');
+                this.pageNoWraper.append('<a class="prevdots" href="javascript:void(0);">...</a>');
             } else if(_begin == 2) {
-                this.pageNoWraper.append('<a href="javascript:void(0);" title="第一页">1</a>');
+                this.pageNoWraper.append('<a class="pageno" data-action="pageno" data-pageno="1" href="javascript:void(0);" title="第1页">1</a>');
             }
             for(var i = _begin;i <= _end; i++) {
                 _pageNoBtn = $('<a>').text(i).attr({
                     href : 'javascript:void(0);',
-                    title : '第' + i + '页'
-                });
+                    title : '第' + i + '页',
+                    "data-pageno" : i,
+                    "data-action" : "pageno"
+                }).addClass('pageno');
                 if(i == _opts.curno)
                     _pageNoBtn.addClass('active');
                 this.pageNoWraper.append(_pageNoBtn);
             }
             if(_end < _opts.total - 1) {
-                this.pageNoWraper.append('<a href="javascript:void(0);" title="">...</a>');
-                this.pageNoWraper.append('<a href="javascript:void(0);" title="">' + _opts.total + '</a>');
+                this.pageNoWraper.append('<a class="nextdots" href="javascript:void(0);">...</a>');
+                this.pageNoWraper.append('<a class="pageno" href="javascript:void(0);" title="第'+ _opts.total + '页" data-action="pageno" data-pageno="'+ _opts.total  +'">' + _opts.total + '</a>');
             } else if(_end == _opts.total - 1) {
-                this.pageNoWraper.append('<a href="javascript:void(0);" title="">' + _opts.total + '</a>');
+                this.pageNoWraper.append('<a class="pageno" href="javascript:void(0);" title="第'+ _opts.total + '页" data-action="pageno" data-pageno="'+ _opts.total  +'">' + _opts.total + '</a>');
             }
         }
     };
@@ -139,52 +146,104 @@
         _nextPageBtn = $('<a>').text(this.lang.nextPageText)
             .attr({
                 href : 'javascript:void(0);',
-                title : this.lang.nextPageTipText
-            });
+                title : this.lang.nextPageTipText,
+                'data-action' : 'next',
+            }).addClass('next');
         if (this.opts.isShowLastPageBtn) {
             _lastPageBtn = $('<a>').text(this.lang.lastPageText)
                 .attr({
                     href : 'javascript:void(0);',
-                    title: this.lang.lastPageTipText
-                });
+                    title: this.lang.lastPageTipText,
+                    "data-action" : "last"
+                }).addClass('last');
         }
         if(this.opts.curno == this.opts.total) {
             _nextPageBtn.addClass('disabled');
             _lastPageBtn && _lastPageBtn.addClass('disabled');
         }
-        _lastPageBtn && this.pageNoWraper.append(_lastPageBtn);
         this.pageNoWraper.append(_nextPageBtn);
+        _lastPageBtn && this.pageNoWraper.append(_lastPageBtn);
     };
 
-    _pro.__onFirstBtnClick = function(_event) {
-        this.opts.curno = 1;
-        this.__updatePageNo();
-        this.opts.onchange();
+
+    _pro.__onPageBtnClick = function(_event) {
+        var _$target= $(_event.target),
+            _action = _$target.data('action');
+        if(!_$target.hasClass('active') && !_$target.hasClass('disabled')) {
+            switch (_action) {
+                case "prev" :
+                    this.opts.curno -= 1;
+                    break;
+                case "next" :
+                    this.opts.curno += 1;
+                    break;
+                case "first" :
+                    this.opts.curno = 1;
+                    break;
+                case "last" :
+                    this.opts.curno = this.opts.total;
+                    break;
+                case "pageno" :
+                    this.opts.curno = _$target.data('pageno');
+                    break;
+                default :
+                    break;
+            }
+            this.__updatePageNo();
+            //this.opts.onchange();
+        }
     };
-
-    _pro.__onLastBtnClick = function(_event) {
-        this.opts.curno = this.opts.total;
-        this.__updatePageNo();
-        this.opts.onchange();
-    };
-
-    _pro.__onPrevBtnClick = function (_event) {
-
-    };
-
-    _pro.__onNextBtnClick = function(_event) {
-
-    }
 
     _pro.__bindEvent = function() {
-        $('.first').bind('click', $.proxy(this.__onFirstBtnClick,this));
-        $('.last').bind('click',$.proxy(this.__onLastBtnClick,this));
-        $('.prev').bind('click',$.proxy(this.__onPrevBtnClick,this));
-        $('.next').bind('click',$.proxy(this.__onNextBtnClick,this));
+        this.pageNoWraper.on('click','a',$.proxy(this.__onPageBtnClick,this));
     };
 
     _pro.__updatePageNo = function() {
+       var _opts = this.opts,
+           _len = $('.pageno',this.pageNoWraper).length,
+           _pageNos = $('.pageno',this.pageNoWraper).filter(function(index){
+               return index != 0 && _len-1 != index;
+           });
+       if(_opts.curno <= 1) {
+           _opts.curno = 1;
+           $('.first,.prev').addClass('disabled');
+       } else {
+           $('.first,.prev').removeClass('disabled');
+       }
+       if(_opts.curno >= _opts.total) {
+           _opts.curno = _opts.total;
+           $('.last,.next').addClass('disabled');
+       } else {
+           $('.last,.next').removeClass('disabled');
+       }
+       if(_opts.total < (this.count * 2 + 3)) {
+            $('.pageno',this.pageNoWraper).removeClass('active');
+            $('[data-pageno="' + _opts.curno + '"]').addClass('active');
+       } else {
+           _begin = _opts.curno - this.count;
+           _end = _opts.curno + this.count;
+           if(_begin < 1) {
+               _end += (1 - _begin);
+               _begin = 1;
+           }
+           if(_end > _opts.total) {
+               _begin = _begin -_end  + _opts.total;
+               _end = _opts.total;
+           }
+           if(_begin > 2) {
 
+           } else if(_begin == 2) {
+
+           }
+           for(var i = _begin;i <= _end; i++) {
+
+           }
+           if(_end < _opts.total - 1) {
+
+           } else if(_end == _opts.total - 1) {
+
+           }
+       }
     };
 
     $.fn.pager = function () {
